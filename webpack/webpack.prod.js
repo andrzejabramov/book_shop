666f9b55fce80fb6a0e715d0a425ed795885cd94
andrejabramov <andrzejvod@gmail.com>
2026-05-03 19:03:30 +0300

    end

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,24 +1,71 @@
 const { merge } = require("webpack-merge");
+const path = require("path");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin"); // 👈 БЫЛО ПРОПУЩЕНО
 
 module.exports = merge(common, {
   mode: "production",
+  devtool: false,
 
-  // Плагин для извлечения CSS в отдельный файл (работает поверх правил из common)
+  output: {
+    filename: "js/[name].[contenthash:8].js",
+    path: path.resolve(__dirname, "../dist"),
+    clean: true,
+    publicPath: "./",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        oneOf: [
+          {
+            resourceQuery: /module/,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          },
+          { use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] },
+        ],
+      },
+    ],
+  },
+
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash].css",
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "public/img",
+          to: "img",
+          globOptions: { ignore: ["**/book-*.png", "**/bookshop.fig"] },
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({ filename: "css/[name].[contenthash:8].css" }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public/index.html"),
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
     }),
   ],
 
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin({ extractComments: false }),
-      new CssMinimizerPlugin(),
-    ],
+    minimizer: [`...`, new CssMinimizerPlugin()],
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
   },
 });
